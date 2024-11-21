0d3f35a5053166b8a75041495ee273f1da5f95ea
Aleksandr Sidorov <aleksandr.sidorov.fe@pm.me>
2024-11-21 22:24:07 +0100

    feat: add CORS to server

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 
+app.use(cors({ origin: ["http://localhost:4200"] }));
 app.use(express.json());
 
 app.post("/register", (req, res) => {
